11b3422020c69e81a651b83ae78ed54d56d2de23
tgrud <tgrud@users.noreply.github.com>
2015-08-15 21:44:03 +0000

    Submit button on edit note template displays feedback during async process.

--- app/assets/javascript/controllers/noteEditController.js
+++ app/assets/javascript/controllers/noteEditController.js
@@ -1,7 +1,12 @@
 angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, $routeParams){
     $scope.note = Note.get({id: $routeParams.id});
+    $scope.isSubmitting = false;
     
     $scope.saveNote = function(note) {
-        note.$update();
+        $scope.isSubmitting = true;
+        
+        note.$update().finally(function(){
+            $scope.isSubmitting = false;
+        });
     }
 });

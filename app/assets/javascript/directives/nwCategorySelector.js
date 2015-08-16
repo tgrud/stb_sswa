a8f025386cf4698a1c206ca61f0847ab4defcc87
tgrud <tgrud@users.noreply.github.com>
2015-08-16 04:47:06 +0000

    Start to add toggle functionality for top categories listed on notes index template.

--- app/assets/javascript/directives/nwCategorySelector.js
+++ app/assets/javascript/directives/nwCategorySelector.js
@@ -4,7 +4,21 @@ angular.module("NoteWrangler").directive('nwCategorySelector', function(Category
         restrict: "E",
         templateUrl: "assets/templates/directives/nwCategorySelector.html",
         link: function(scope, element, attrs){
+            var activeCateogry = {};
             scope.categories = Category.query();
+            
+            scope.isActive = function(category){
+                return activeCateogry.id === category.id;
+            }
+            
+            scope.toggleCategory = function(category){
+                if(category === activeCateogry) {
+                    activeCateogry = {};
+                } else {
+                    activeCateogry = category;   
+                }
+            }
+            
         }
     };
 });

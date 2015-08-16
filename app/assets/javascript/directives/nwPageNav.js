0277407d13e31ee02ce0c5405d4a26969d626c61
tgrud <tgrud@users.noreply.github.com>
2015-08-16 03:09:17 +0000

    Add active style functionality to navigation.

--- app/assets/javascript/directives/nwPageNav.js
+++ app/assets/javascript/directives/nwPageNav.js
@@ -2,6 +2,11 @@ angular.module("NoteWrangler").directive('nwPageNav', function(){
     return {
         replace: true,
         restrict: "E",
-        templateUrl: "assets/templates/directives/nwPageNav.html"
+        templateUrl: "assets/templates/directives/nwPageNav.html",
+        controller: function($scope, $location){
+            $scope.isPage = function(name){
+                return new RegExp("/" + name + "($|/)").test($location.path());
+            };
+        }
     };
 });
